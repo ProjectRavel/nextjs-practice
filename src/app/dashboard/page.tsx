b9e2873257a44f2699be1b1915c400532c3fc705
ProjectRavel <rafaelcodeid@gmail.com>
2025-06-14 23:15:14 +0700

    Add MongoDB and Mongoose dependencies; implement database connection

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,19 @@
-export default function Dashboard(){
-    return (
-        <div>
-            <h1 className="text-5xl font-bold">Dashboard</h1>
-        </div>
-    )
+"use client";
+import useSWR from "swr";
+
+export default function Dashboard() {
+  const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
+  const { data, error, isLoading } = useSWR(
+    "https://jsonplaceholder.typicode.com/posts",
+    fetcher
+  );
+
+  console.log(data)
+
+  return (
+    <div>
+      <h1 className="text-5xl font-bold">Dashboard</h1>
+    </div>
+  );
 }
