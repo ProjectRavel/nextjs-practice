7f712bdfdd5d8c847f85b4d27a9170cae639a661
ProjectRavel <rafaelcodeid@gmail.com>
2025-06-18 22:08:35 +0700

    Implement NextAuth with Google and Credentials providers for authentication

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,9 @@
 import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
+import connect from "./utils/db";
+import User from "./models/User";
+import bcrypt from "bcryptjs";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
@@ -7,6 +11,49 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       clientId: process.env.AUTH_GOOGLE_ID!,
       clientSecret: process.env.AUTH_GOOGLE_SECRET!,
     }),
+    Credentials({
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+
+      async authorize(
+        credentials: Partial<Record<"email" | "password", unknown>>
+      ) {
+        await connect();
+        const email = credentials?.email as string | undefined;
+        const password = credentials?.password as string | undefined;
+
+        if (!email || !password) {
+          throw new Error("Email and password are required");
+        }
+
+        const user = await User.findOne({ email });
+
+        if (user) {
+          const isPasswordCorrect = await bcrypt.compare(
+            password,
+            user.password
+          );
+
+          if (isPasswordCorrect) {
+            return user;
+          } else {
+            throw new Error("Invalid password");
+          }
+        } else {
+          throw new Error("User not found");
+        }
+      },
+    }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    strategy: "jwt",
+  },
+  pages: {
+    error: "/login",
+    signOut: "/",
+    signIn: "/login",
+  },
 });
